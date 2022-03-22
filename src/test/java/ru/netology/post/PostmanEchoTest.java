6754d715dcad589baa30951f0365a94519ab725a
IvanNetology <ivan.ivan.91.91@internet.ru>
2022-03-22 13:16:03 +0300

    initial commit

---
+++ src/test/java/ru/netology/post/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+package ru.netology.post;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldPostSomeData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+            .baseUri("https://postman-echo.com")
+            .contentType("text/plain; charset=UTF-8")
+            .body("розовый акваланг") // отправляемые данные (заголовки и query можно выставлять аналогично)
+        // Выполняемые действия
+        .when()
+            .post("/post")
+        // Проверки
+        .then()
+            .statusCode(200)
+            .body("data", equalTo("розовый акваланг"))
+        ;
+    }
+}
