b25b11ac640dc4c6c1e4ad3580a30cb314be188b
IvanNetology <ivan.ivan.91.91@internet.ru>
2022-03-22 13:33:58 +0300

    correct body

--- src/test/java/ru/netology/post/PostmanEchoTest.java
+++ src/test/java/ru/netology/post/PostmanEchoTest.java
@@ -14,7 +14,7 @@ public class PostmanEchoTest {
         given()
             .baseUri("https://postman-echo.com")
             .contentType("text/plain; charset=UTF-8")
-            .body("розовый слон") // отправляемые данные (заголовки и query можно выставлять аналогично)
+            .body("розовый акваланг") // отправляемые данные (заголовки и query можно выставлять аналогично)
         // Выполняемые действия
         .when()
             .post("/post")
